c5666c07690e5bf173c4fca3893811e11953ef26
ganzhenchao <ganzhenchao@ubuntu.(none)>
2013-11-15 14:26:21 +0800

    small change

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 flex lexya_e.l
 bison -d lexya_e.y
-gcc -g -o graph lexya_e.tab.c lex.yy.c liwei.c 
+gcc -g -o graph lexya_e.tab.c lex.yy.c graph.c 
 gcc -g -o parser lexya_e.tab.c lex.yy.c parser.c
